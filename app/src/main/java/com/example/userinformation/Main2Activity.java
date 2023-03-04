8ddc1d5e2b5f13c376f2802bd8343517f106771b
Reema <121347238+reemamaher201@users.noreply.github.com>
2023-03-04 09:34:17 +0200

    second command

--- app/src/main/java/com/example/userinformation/Main2Activity.java
+++ app/src/main/java/com/example/userinformation/Main2Activity.java
@@ -18,9 +18,7 @@ import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.badge.BadgeUtils;
-import com.google.firebase.firestore.DocumentReference;
+
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
